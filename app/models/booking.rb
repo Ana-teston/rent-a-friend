7f982238291e1fd0392e863191c255f2bbce8421
Alexis Sakabenis <alexis.10@outlook.be>
2023-06-06 15:07:18 +0200

    Add validation for Friend and Booking model, waits for reviews model to be finish to add it validation too

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,17 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :friend
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :start_date_before_end_date, presence: true
+  validates :activity, presence: true
+
+  def start_date_before_end_date
+    return if start_date.blank? || end_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "The end date can not be before the start date")
+    end
+  end
 end
