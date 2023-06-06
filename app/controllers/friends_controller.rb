ee3c010b161290eab0be0cec418c943912d84fec
Guerfi Hamza <hmzaa600@gmail.com>
2023-06-06 16:29:47 +0100

    added policies for friend controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,6 @@
 class FriendsController < ApplicationController
   def index
-    @friends = Friend.all
+    @friends = policy_scope(Friend)
   end
 
   def show
@@ -10,21 +10,44 @@ class FriendsController < ApplicationController
     @reviews = Review.all
     @review.friend = @friend
     @review.booking = @booking
+    authorize @friend
+  end
+
+  def edit
+    @friend = Friend.find(params[:id])
+    authorize @friend
   end
 
   def new
     @friend = Friend.new
+    authorize @friend
   end
 
+  def update
+    @friend = Friend.find(params[:id])
+    @friend.update(friends_param)
+    authorize @friend
+    redirect_to friend_path(@friend), notice:"updated"
+  end
   def create
     @friend = Friend.new(friends_param)
     @friend.user = current_user
+
+    authorize @friend
 
     if @friend.save
       redirect_to friend_path(@friend), notice: "Friend created"
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @friend = Friend.find(params[:id])
+    @friend.destroy
+
+    authorize @friend
+    redirect_to friends_path, notice: "Deleted"
   end
 
   private
