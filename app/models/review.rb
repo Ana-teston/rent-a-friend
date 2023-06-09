7b3c841bcce9a82357146e474117ecb7ff526305
Guerfi Hamza <hmzaa600@gmail.com>
2023-06-09 14:12:54 +0100

    fixed latest bugs

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :friend
   belongs_to :booking
-
+  validates :title, :body, :rating, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
